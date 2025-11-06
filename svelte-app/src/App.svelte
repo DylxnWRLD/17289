88f9051b821e361fbe41138e6ee6e9a725dfdfcf
DylxnWRLD <dylaniscute.23@gmail.com>
2025-11-06 13:06:45 -0600

    Svelte

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+	import Hijo from './Hijo.svelte';
+	export let name;
+	let nombre;
+
+
+	let contador = 0;
+	function incrementar(){
+		contador = contador + 1;
+	}
+
+	function saludar(){
+		alert(nombre ? nombre : "nulo")
+	}
+</script>
+
+<main>
+	<p>Adios {name}!</p>
+	<h1>Contar: {contador}</h1>
+	<button on:click={incrementar}>Contar++</button>
+
+	<button on:click={saludar}>Saludar</button>
+	<input bind:value={nombre} placeholder="Tu nombre">
+
+	<Hijo color="red" texto="Guardar"></Hijo>
+	<Hijo texto="Borrar"></Hijo>
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ svelte-app/src/Hijo.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let texto = "Botón";
+    export let color = "blue";
+
+</script>
+
+<!-- markup html -->
+ <button style="background-color: {color};">
+    {texto}
+ </button>
+
+<style>
+    button {
+        color: white;
+    }
+</style>
